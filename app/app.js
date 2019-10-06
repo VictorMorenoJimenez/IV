ed60b8efbee62a6b4ad7f03b9e0c2a17f20e4cfc
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-10-06 02:06:20 +0200

    Added some routes and end points definitions

--- app/app.js
+++ app/app.js
@@ -1,13 +1,27 @@
 'use strict';
-
 const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+//Rutes
+const countryRoutes = require('../routes/country');
+const stateRoutes = require('../routes/state');
+const cityRoutes = require('../routes/city');
+
+app.use('/country', countryRoutes);
+app.use('/state', stateRoutes);
+app.use('/city', cityRoutes);
+
+
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
