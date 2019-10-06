25d34abb54f2d4b17e0dae854d8680ef5ed69725
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-10-06 19:09:52 +0200

    Defined and implemented get methods, post methods TODO

--- routes/country.js
+++ routes/country.js
@@ -1,34 +1,58 @@
 const express = require('express');
 const router = express.Router();
-
-var cities = require('../models/provisionalDB');
-var countries = require('../models/provisionalDB');
-var states = require('../models/provisionalDB');
+require('../models/provisionalDB');
 
 /*************************************************** GET ***********************************************************/
-
-router.get('/', (req, res) => {
-  res.send(global.countries[0].name);
-});
   
-router.get('/states', (req, res) => {
-  res.send('This is supposed to give the holidays of all the states of the country \n');
+router.get('/states/:country_name', (req, res) => {
+  //res.send('This is supposed to give the holidays of all the states of the country \n');
+  _country = req.params.country_name;
+  exists = false;
+  _holidays = [];
+
+  for (state of global.states){
+    if(state.country == _country){
+      exists = true;
+        for(holiday of state.holidays){
+          _holidays.push(holiday);
+        }
+    }
+  }
+  res.setHeader('Content-Type','application/json');
+  exists ? res.status(200).send(JSON.stringify(_holidays)) : res.status(404).send("Not found");
+  
+
 });
 
 router.get('/:name', (req, res) => {
-  res.send('This is supposed to give the holidays from the country named ' + req.params.name);
+  //res.send('This is supposed to give the holidays from the country named ' + req.params.name);
+  _country = req.params.name;
+  _holidays = [];
+  exists=false;
+
+  for (country of global.countries){
+    if(country.name == _country){
+      _holidays = country.holidays;
+      exists = true;
+      break;
+    }
+  }
+  res.setHeader('Content-Type','application/json');
+  exists ? res.status(200).send(JSON.stringify(country.holidays)) : res.status(404).send("Not found");
 });
 
 /*************************************************** POST ***********************************************************/
 
 router.post(
   '/new',(req, res) => {
-    console.log(req.body)
+    console.log(req.body);
+    //TODO
   }
 );
 
 router.post('/:name', (req, res) => {
   res.send('Create new holidays to country with name :name \n');
+  //TODO
 });
 
 module.exports = router;
