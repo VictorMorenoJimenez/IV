c29a356328dcba888e0a3cd8e11f5c80d705b810
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-10-08 16:52:57 +0200

    Created test for endpoint GET /country, working on #16

---
+++ test/country.js
@@ -0,0 +1,56 @@
+//Test envirorment
+
+process.env.NODE_ENV = 'test';
+
+let mongoose = require("mongoose");
+let Country = require("../models/country");
+
+let chai = require("chai");
+let chaiHttp = require('chai-http');
+let server = require('../app/app');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+
+describe('Country', () => {
+    beforeEach((done) => { 
+        //TODO
+        console.log("Running test");
+        done();                  
+    });
+
+  describe('GET /country', () => {
+      it('should get all the countries of database', (done) => {
+        chai.request(server)
+            .get('/country')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+              done();
+            });
+      });
+  });
+
+});
+
+describe('Country', () => {
+    beforeEach((done) => { 
+        //TODO
+        console.log("Running test");
+        done();                  
+    });
+
+  describe('POST /country/new', () => {
+      it('should create new country', (done) => {
+        chai.request(server)
+            .get('/country')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+              done();
+            });
+      });
+  });
+
+});
