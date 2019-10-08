0cdd1540145bfe530af2765bd92e4af890ce7668
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-10-08 23:10:34 +0200

    Added 1 new test for POST /country/new

--- test/country.js
+++ test/country.js
@@ -14,11 +14,60 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('Country', () => {
-    /*beforeEach((done) => { 
-        //TODO
-        console.log("Running test");
-        done();                  
-    });*/
+  beforeEach((done) => { 
+    Country.deleteOne({}, (err) => { 
+      done();           
+   });      
+  });
+
+  describe('POST /country/new', () => {
+      it('should create new country', (done) => {
+        let n_country = {
+          name: "Spain",
+          holidays: [
+            {
+              day: 1,
+              month: 1,
+              description: "Test"
+            }
+          ],
+          states: [
+            "State test1",
+            "State test2"
+          ]
+        }
+
+        chai.request(server)
+            .post('/country/new')
+            .send(n_country)
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.have.property('message')
+                  .eql('Country successfully added!');
+              done();
+            });
+      });
+  });
+
+  describe('POST /country/new', () => {
+    it('should not create country without name', (done) => {
+      let n_country = {
+        states: [
+          "State test1",
+          "State test2"
+        ]
+      }
+
+      chai.request(server)
+          .post('/country/new')
+          .send(n_country)
+          .end((err, res) => {
+                res.body.should.have.property('message')
+                .eql('Country validation failed: name: Path `name` is required.');
+            done();
+          });
+    });
+});
 
   describe('GET /country', () => {
       it('should get all the countries of database', (done) => {
@@ -34,25 +83,3 @@ describe('Country', () => {
 
 });
 
-/*describe('Country', () => {
-    beforeEach((done) => { 
-        //TODO
-        console.log("Running test");
-        done();                  
-    });
-
-  describe('POST /country/new', () => {
-      it('should create new country', (done) => {
-        chai.request(server)
-            .post('/country')
-            .send(country)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  //res.body.should.be.a('array');
-              done();
-            });
-      });
-  });
-
-});*/
