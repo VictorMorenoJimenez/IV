9d840a56ffaa92e71853caf903b9ebc13b255875
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-10-22 22:30:56 +0200

    Changed error messages from joi

--- models/joi.js
+++ models/joi.js
@@ -1,35 +1,41 @@
 const Joi = require('joi'); 
 
+let holidays_ = Joi.object().keys({
+    day: Joi.number().required(),
+    month: Joi.number().required(),
+    description: Joi.string().required()
+})
+
 const countryValidateSchema = Joi.object().keys({
-    name: Joi.string().required(),
-    holidays: Joi.array().items(Joi.object().keys({
-        day: Joi.number(),
-        month: Joi.number(),
-        description: Joi.string()
-    })),
-    states: Joi.array().items(Joi.string())
+    name: Joi.string().required()        .error(() => {
+        return {
+          message: 'Name is required and must be String',
+        };
+      }),
+    holidays: Joi.array().items(holidays_).required(),
+    states: Joi.array().items(Joi.string().required())
 });
 
 const stateValidateSchema = Joi.object().keys({
-    name: Joi.string().required(),
-    holidays: Joi.array().items(Joi.object().keys({
-        day: Joi.number(),
-        month: Joi.number(),
-        description: Joi.string()
-    })),
-    country: Joi.string(),
-    cities: Joi.array().items(Joi.string())
+    name: Joi.string().required()        .error(() => {
+        return {
+          message: 'Name is required and must be String',
+        };
+      }),
+    holidays: Joi.array().items(holidays_).required(),
+    country: Joi.string().required(),
+    cities: Joi.array().required().items(Joi.string())
 });
 
 const cityValidateSchema = Joi.object().keys({
-    name: Joi.string().required(),
-    holidays: Joi.array().items(Joi.object().keys({
-        day: Joi.number(),
-        month: Joi.number(),
-        description: Joi.string()
-    })),
-    country: Joi.string(),
-    state: Joi.string()
+    name: Joi.string().required()        .error(() => {
+        return {
+          message: 'Name is required and must be String',
+        };
+      }),
+    holidays: Joi.array().items(holidays_).required(),
+    country: Joi.string().required(),
+    state: Joi.string().required()
 });
 
 module.exports = { countryValidateSchema, stateValidateSchema, cityValidateSchema };
