3a64af9f59a8e76c02386c3941a211750c395ef1
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-11-05 23:54:23 +0100

    Return state's country holidays when ask for states holidays closes #60

--- app/routes/state.js
+++ app/routes/state.js
@@ -1,4 +1,5 @@
 let State = require('../../models/state');
+let Country = require('../../models/country');
 let validate = require('../../models/joi');
 const Joi = require('joi')
 
@@ -28,18 +29,52 @@ function getStates(req, res) {
 function getStatebyName(req, res) {
     state_name = req.params.state_name;
     let query = State.find({name: state_name}, {holidays: 1, _id: 0})
+    let query_n = State.find({name: state_name}, {country: 1, _id: 0})
+    let n_country;
+    let holidays_country;
 
-    query.exec( (err, state) =>{
+    query_n.exec( (err, state) =>{
         //Check if no errors and send json back
         if(err){
             console.log(err);
             res.send(err);
         }else{
-            console.log("GET /state/" + state_name + ". Get the holidays from the state " + state_name);
-            res.status(200).json(state);
+            n_country = state[0].country;
+
+            let query_country = Country.find({name: n_country}, {holidays: 1, _id: 0})
+
+            query_country.exec( (err, country_holidays) =>{
+                //Check if no errors and send json back
+                if(err){
+                    console.log(err);
+                    res.send(err);
+                }else{
+                    holidays_country = country_holidays;
+
+                    query.exec( (err, state) =>{
+                        //Check if no errors and send json back
+                        if(err){
+                            console.log(err);
+                            res.send(err);
+                        }else{
+                            console.log("GET /state/" + state_name + ". Get the holidays from the state " + state_name);
+                            res.status(200).json(state.concat(holidays_country));
+                        }
+                    })
+                }
+            })
         }
-        
     })
+
+
+
+    
+
+    
+
+
+
+
 }
 
 /**
