c261044045b8646896855fb1f75eb799f5d26264
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-11-07 19:55:04 +0100

    Testing

--- test/country.js
+++ test/country.js
@@ -153,7 +153,7 @@ describe('PUT /country/state/:country_name', () => {
   });
 
   describe('GET /country/:country_name', () => {
-    it('should get the holidays from the country :country_name',(done) => {
+    it('should get the holidays from the country :country_name',async (done) => {
         /*chai.request(server)
         .get('/country/Spain')
         .end((err, res) => {
