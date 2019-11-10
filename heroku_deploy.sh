0595832615a33840a51cd5230789c83c315faf39
Victor Moreno Jimenez <victormoreno@correo.ugr.es>
2019-11-10 23:04:30 +0100

    Added heroku create app

---
+++ heroku_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Log in the web browser"
+heroku login
+echo "Log in the container of heroku"
+heroku container:login
+echo "@@@@@@@@@@@@@@@@@@@@@ Building image @@@@@@@@@@@@@@@@@@@"
+docker build -t web .
+echo "@@@@@@@@@@@@@@@@@@@@@ Pushing imageto heroku container @@@@@@@@@@@@@@@@@@@"
+heroku container:push web
+echo "@@@@@@@@@@@@@@@@@@@@@ Set node ENV @@@@@@@@@@@@@@@@@@@"
+heroku config:set NODE_ENV='HEROKU'
+echo "@@@@@@@@@@@@@@@@@@@@@ Release and Scale @@@@@@@@@@@@@@@@@@@"
+heroku container:release web
+heroku ps:scale web=1
+echo "@@@@@@@@@@@@@@@@@@@@@ Openning browser @@@@@@@@@@@@@@@@@@@"
+heroku open
